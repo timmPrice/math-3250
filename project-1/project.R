e869757aab8f8902dec797d80b1d7f97c0c1e6fc
timmPrice <timmPrice6@gmail.com>
2026-01-16 16:32:07 -0500

    progress on proj 1

--- project-1/project.R
+++ project-1/project.R
@@ -2,19 +2,22 @@
 
 gdp <- read.csv("./gdp.csv", header = TRUE, sep = ",", fill = TRUE)
 
+# Q1 A
 
-# Q1 A 
+gdp$PERCAPGDP <- gdp$X2015Q2 / gdp$X2015pop
 
-gdp$PERCAPGDP <-gdp$X2015Q2 / gdp$X2015pop
-
-# Q1 B 
+# Q1 B
 
 write.table(x=gdp, file = "./gdp1.csv", sep = "!", row.names = F)
+print(gdp)
 
 # Q2
 
+gdp_total <- sum(gdp$X2016Q2, na.rm = TRUE)
+print(gdp_total)
 
+# Q3
+# gdp over 500,000
+ff <- gdp[gdp$`X2015Q2` >= 500000, ]
+print(ff)
 
-
-
-gdp
